d4ee77f8e93bdf8db4e58da50562abc474522c39
league <maddoxseul@sbsd.net>
2023-10-27 18:21:04 -0700

    asdf

---
+++ src/_04_animals_inheritance/Animal.java
@@ -0,0 +1,13 @@
+package _04_animals_inheritance;
+
+public class Animal {
+	String name; 
+	String color;
+	boolean isGirl;
+Animal(String name, String color, boolean isGirl){
+	this.name=name;
+	this.color=color;
+	this.isGirl=isGirl;
+	
+}
+}
--- src/_04_animals_inheritance/Dog.java
+++ src/_04_animals_inheritance/Dog.java
@@ -1,5 +1,7 @@
 package _04_animals_inheritance;
 
-public class Dog {
-	
+public class Dog extends Animal{
+	Dog(String name, String color, boolean isGirl){
+		super(name,color,isGirl);
+	}
 }
